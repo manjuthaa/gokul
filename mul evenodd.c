0214935b106108e1aff282bfff940ece92b036dd
manjuthaa <31506174+manjuthaa@users.noreply.github.com>
2018-01-21 16:05:21 +0530

    Create mul evenodd.c

---
+++ mul evenodd.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	int a=9,b=2;
+	int c=a*b;
+	printf("value of c:%d\n",c);
+	if(c%2==0)
+	{
+		printf("the value is even");
+		
+	}
+else
+{
+	printf("the value is odd");
+}
+return 0;
+}

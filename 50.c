41a1c4d19427e0e204e3999e8099ef990a380608
manjuthaa <31506174+manjuthaa@users.noreply.github.com>
2018-02-22 10:56:59 +0000

    Create 50.c

---
+++ 50.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	printf("%d",n);
+	if(n%2==0)
+	{
+		printf("\nyes");
+			}
+	else
+	{
+		printf("\nno");
+	}
+	return 0;
+}

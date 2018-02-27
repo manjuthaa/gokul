6e61b3bb3a6baf7967725435cc427832ca9e5eb5
manjuthaa <31506174+manjuthaa@users.noreply.github.com>
2018-02-27 10:28:34 +0000

    Create 95.c

---
+++ 95.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+	int p,n,r,a;
+            scanf("%d %d %d",&p,&n,&r);
+            printf("%d\n%d\n%d",p,n,r);
+            a=(p*n*r)/100;
+            printf("\n%d",a);
+            return 0;
+}
+	
